43378118e59a594608362089b8363a8460f63c7b
gunilla <gunilla1148@gmail.com>
2017-06-04 17:54:23 +0800

    fixed orders bug

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,15 @@ Rails.application.routes.draw do
   namespace :admin do
      resources :products
      resources :orders do
+
           member do
             post :cancel
             post :ship
             post :shipped
             post :return
           end
-        end   end
+        end
+ end
    resources :products do
      member do
        post :add_to_cart
